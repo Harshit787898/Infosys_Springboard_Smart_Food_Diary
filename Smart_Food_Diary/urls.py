351e3d3c7d81cf0f7adc3517dd4ab4b2ed8ece4b
Harshit787898 <singh.787898.harshit@gmail.com>
2024-11-26 16:42:31 +0530

    changes made

--- Smart_Food_Diary/urls.py
+++ Smart_Food_Diary/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
 from authentication import views
@@ -21,4 +22,5 @@ urlpatterns = [
     path('',views.SignupPage,name='signup'),
     path('login/', views.LoginPage, name='login'),
     path('home/', views.HomePage, name='home'),
+    path('logout/', views.LogoutPage, name='logout'),
 ]
